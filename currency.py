576ee6b58375f88092d60fa2bb84de521b250453
ilangrin <152694435+ilangrin@users.noreply.github.com>
2023-12-14 13:59:19 +0200

    end

--- currency.py
+++ currency.py
@@ -1,29 +1,45 @@
 import requests
 
-
 API_KEY = 'fca_live_6LIM6kXb5yI7XZ4PzxA5URQXaFgN2clXlr1cnECP'
 BASE_URL = f"https://api.freecurrencyapi.com/v1/latest?apikey={API_KEY}"
 CURRENCIES = ["USD", "EUR", "ILS", "CAD", "AUD"]
+
 def currency_converter(base):
-    currencies ="," .join(CURRENCIES)
+    currencies = ",".join(CURRENCIES)
     url = f"{BASE_URL}&base_currency={base}&currencies={currencies}"
     try:
         response = requests.get(url)
         data = response.json()
         return data["data"]
-    except Exception as  e:
-        print(e)
+    except:
+        print("Error in getting data.")
         return None
 
-base =input("enter the base currencies (q for quit): ").upper()
+while True:
+    base = input("Enter the base currency (q for quit, c for calculator): ").upper()
 
-if base not in CURRENCIES:
-    print("invalid currency")
-
-data = currency_converter(base)
-del data[base]
-for ticker, value in data.items():
-    print((f"{ticker}: {value}"))
-
-
-
+    if base == "Q":
+        break
+    elif base == "C":
+        base = input("Enter the base currency for conversion: ").upper()
+        if base not in CURRENCIES:
+            print("Invalid currency.")
+            continue
+        amount_str = input("Amount to convert: ")
+        if not amount_str.isdigit():
+            print("Invalid amount.")
+            continue
+        amount = int(amount_str)
+        data = currency_converter(base)
+        if not data:
+            continue
+        for ticker, rate in data.items():
+            converted_amount = amount * rate
+            print(f"{amount} {base} is {converted_amount} {ticker}")
+    else:
+        data = currency_converter(base)
+        if not data:
+            continue
+        del data[base]
+        for ticker, value in data.items():
+            print(f"{ticker}: {value}")
